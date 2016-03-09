e53b13c3c0f37e38abb58be65b0488abe812a1f1
高山　圭一 <uu084383@CA0516.local>
2016-03-09 19:05:26 +0900

    initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*$(function(){//$(function(){});はhtmlを読み込んでから処理を実行するという意味//
+	$("#contents1").each(function(){
+		$(this).find("li:gt(3)").each(function(){$(this).hide();});
+		$(this).append('<p>» もっと見る</p>');
+		$(this).find("p:last").click(function(){$(this).parent().find("li").show(6); $(this).remain();});
+		$(this).find("li:gt(3)").each(function(){$(this).hide();});
+		$(this).append('<p>» 元に戻す</p>');
+		$(this).find("li:gt(3)").click(function(){$(this).parent().find("li").show(3); $(this).remove();});
+	});
+});*/
+
+$(function(){
+	$("#contents1").each(function(){
+		$(this).find("li:gt(2)").each(function(){$(this).hide();});
+		$(this).append('<p>もっと見る</p>');
+		$(this).find("p:last").click(function(){$(this).parent().find("li").show(1); $(this).remain();});
+	});
+});
