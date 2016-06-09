d2fe0c3ee5c8de9c04f34f9c3d8a861125fe978f
michio <highberg525@gmail.com>
2016-06-09 21:11:21 +0900

    「もっとみる」「戻る」ボタンの設置とそのアクションをjavaで表現中

--- js/list.js
+++ js/list.js
@@ -1,36 +1,15 @@
-//$(function(){});はhtmlを読み込んでから処理を実行するという意味
-$(function(){
-	$(".list").each(function(){
-		$(this).find("li:gt(2)").each(function(){$(this).hide();});//最初に表示する項目数選択
-		$(this).append('<p>>>もっと見る</p>');//最初に表示させるボタン設置
-		$(this).find("p:last").click(function(){$(this).parent().find("li").show(1); $(this).remain();});//ボタンを押したらどうなるのか
-//		$(this).css('display','none');//押したボタンを非表示に
-		$(this).append('<p>>>閉じる</p>');//次のボタンを表示させる
-	});
-});
-
-/*
-$(function(){
-	$(".list").each(function(){
-		$(this).find("li:gt(2)").each(function(){$(this).hide();});//最初に表示する項目数選択
-		$(this).append('<p>>>もっと見る</p>');//最初に表示させるボタン設置
-		$(this).find("p:last").click(function(){
-			$(this).parent().find("li").show(1);
-			$(this).remain();
-		});//ボタンを押したらどうなるのか
-		$(this).text('閉じる').addClass('close');
-	});
-});
-$(function () {
-		$(this).find("li:gt(2)").each(function(){$(this).hide();});
-		$(this).click(function () {
-				if ($(this).find("li:gt(2)").each(function(){$(this).hide();});{
-					$(this).prevAll().slideDown();
-					$(this).text('閉じる').addClass('close');
-				} else {
-					$(this).prevAll().slideUp();
-					$(this).text('もっと見る').removeClass('close');
-				}
-		});
-		});
+/*michi_homeworkのlist.html用javascriptです
+$(function(){});はhtmlを読み込んでから処理を実行するという意味
 */
+
+$(function () {
+	$(".list").append("<p>>>もっと見る</p>");
+	$(".list").find("li:gt(2)").click(function(){
+		$(".list").hide();
+	});
+	$("<p>>>もっと見る</p>").click(function(){
+		$(".list").slideDown();
+		$(".list").removeClass("close");
+		$(".list").text("閉じる");
+	});
+});
