d78c51231b3cf624dc17307a2e3599ab5b469864
michio <highberg525@gmail.com>
2016-06-13 21:05:38 +0900

    list.jsを修正しました

--- js/list.js
+++ js/list.js
@@ -1,15 +1,22 @@
 /*michi_homeworkのlist.html用javascriptです
 $(function(){});はhtmlを読み込んでから処理を実行するという意味
 */
-
-$(function () {
-	$(".list").append("<p>>>もっと見る</p>");
-	$(".list").find("li:gt(2)").click(function(){
-		$(".list").hide();
-	});
-	$("<p>>>もっと見る</p>").click(function(){
-		$(".list").slideDown();
-		$(".list").removeClass("close");
-		$(".list").text("閉じる");
+$(function(){
+	$('.list').each(function(){
+		var $ele = $(this);
+		$ele.prepend('<div class="open"><a href="#">&#9660; >>続きを読む</a></div>');
+		$ele.append('<div class="close"><a href="#">&#9650; >>閉じる</a></div>');
+		$ele.find('.open').nextAll().hide();
+		$ele.find('.open').click(function(){
+			$ele.find('.open').hide();
+			$ele.find('.open').nextAll().slideDown();
+			return false;
+		});
+		$ele.find('.close').click(function(){
+			$ele.find('.open').nextAll().slideUp('slow', function(){
+				$ele.find('.open').show();
+		});
+		return false;
+		});
 	});
 });
