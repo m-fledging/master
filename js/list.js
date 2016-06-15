a3ed4ca0eb8d04c2276f104bdf3b7806896e34c0
michio <highberg525@gmail.com>
2016-06-15 20:29:50 +0900

    【一付】list.htmlはlist内に56,75,76行目を追加。list.htmlは全体書き直全体書き直し。

--- js/list.js
+++ js/list.js
@@ -1,22 +1,23 @@
 /*michi_homeworkのlist.html用javascriptです
 $(function(){});はhtmlを読み込んでから処理を実行するという意味
 */
+
 $(function(){
-	$('.list').each(function(){
-		var $ele = $(this);
-		$ele.prepend('<div class="open"><a href="#">&#9660; >>続きを読む</a></div>');
-		$ele.append('<div class="close"><a href="#">&#9650; >>閉じる</a></div>');
-		$ele.find('.open').nextAll().hide();
-		$ele.find('.open').click(function(){
-			$ele.find('.open').hide();
-			$ele.find('.open').nextAll().slideDown();
-			return false;
+		$('.more').each(function(){
+				var $ele = $(this);
+				$ele.prepend('<div class="open"><a href="#">&#9660; もっと見る</a></div>');
+				$ele.append('<div class="close"><a href="#">&#9650; 閉じる</a></div>');
+				$ele.find('.open').nextAll().hide();
+				$ele.find('.open').click(function(){
+						$ele.find('.open').hide();
+						$ele.find('.open').nextAll().slideDown();
+						return false;
+				});
+				$ele.find('.close').click(function(){
+						$ele.find('.open').nextAll().slideUp('slow', function() {
+								$ele.find('.open').show();
+						});
+						return false;
+				});
 		});
-		$ele.find('.close').click(function(){
-			$ele.find('.open').nextAll().slideUp('slow', function(){
-				$ele.find('.open').show();
-		});
-		return false;
-		});
-	});
 });
