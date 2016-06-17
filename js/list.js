620cde9345fe0dada389d30aaf59b406fd901ed7
michio <highberg525@gmail.com>
2016-06-17 21:19:16 +0900

    「もっと見る」と「閉じる」をhtml内に組み込み、制御させる件

--- js/list.js
+++ js/list.js
@@ -1,7 +1,8 @@
 /*michi_homeworkのlist.html用javascriptです
 $(function(){});はhtmlを読み込んでから処理を実行するという意味
 */
-
+//	「もっと見る」「閉じる」をjQueryに書くパターン
+/*
 $(function(){
 		$('.more').each(function(){
 				var $ele = $(this);
@@ -21,3 +22,75 @@ $(function(){
 				});
 		});
 });
+*/
+
+$(function(){
+		$('.list').each(function(){
+				$(this).find('li:gt(2)').hide();
+				return false;
+				});
+				$('.more a').each(function(){
+						$(this).find('.more a').on('click', function(){
+								$(this).find('.more a').hide();
+								$(this).find('.more a').nextAll().slideDown();
+								return false;
+						});
+						$(this).find('.close a').on('click', function(){
+								$(this).find('.more a').nextAll.slideUp('slow', function() {
+										$(this).find('.more a').show();
+								});
+								return false;
+						});
+				});
+});
+
+
+
+//	以下はhtmlに「もっと見る」「閉じる」を書いてjQueryにはその制御のみ書くパターン
+
+/*
+$(function(){
+		$(".more").on('click',function(){
+				$(".more").show(2);				//.show() == css("display", "none");
+				
+			});
+		$(".close").on('click',function(){
+				$(".close").hide();			//.hide() == css("display", "none");
+				
+			});
+});
+
+$(function(){
+		$(".list").each(function(){
+				$(".more").find("li:gt(5)").on('click',function(){
+						
+				each(function(){$(this).hide();});
+				$(this).append('<p>もっと見る</p>');
+				$(this).find("p:last").click(function(){$(this).parent().find("li").show(1); $(this).remain();});
+		});
+});
+
+		
+		
+		
+		$(function(){
+			$(".list").find("li:gt(2)");
+				$(".more a").on('click',function(){
+						$(".list").show(5);
+						$(".more a").hide('txt', >>もっと見る);
+						$(".close a").append('<a>>>閉じる</a>');
+				});
+				$(".close a").on('click',function(){
+						$(".close").hide();
+						
+				});
+		});
+		
+
+
+$(function(){
+		$("#contents .list").find("li:gt(2)");
+	});
+
+*/
+
