7a74f7524ff9fea3e199a985769c9fc417bc22c5
michio <highberg525@gmail.com>
2016-06-21 20:29:02 +0900

    「もっとみる」「閉じる」修正途中

--- js/list.js
+++ js/list.js
@@ -28,48 +28,47 @@ $(function(){
 
 
 //	以下はhtmlに「もっと見る」「閉じる」を書いてjQueryにはその制御のみ書くパターン
+
+$(function(){
+		$(".list").each(function(){//.listは以下のような制御を持つ
+				$(".list").find("li:gt(2)").hide();//.listからliを３行表示させ、他のliは隠す
+				return false;//出来るまで繰り返す
+				});
+		$(".more").each(function(){//.moreは以下のような制御を持つ
+				$(".more").on('click', function(){//.moreをクリックすると以下の制御を持つ
+						$(".more").find("li:gt(5)");//liを６行表示させる
+						$(".more").find(".close");//.closeを表示させる
+						return false;//出来るまで繰り返す
+						});
+				$(".more").find(".close").on('click', function(){
+						$(this).find('.more').nextAll.slideUp('slow', function(){
+								$(this).find('.more').show();
+						});
+								return false;
+						});
+				});
+});
+
 /*
 $(function(){
-		$('.list').each(function(){//.listは以下のような制御を持つ
-				$('.list').find('li:gt(3)').hide();//.listからliを３行表示させる
-				return false;//出来るまで繰り返す
-				});
-		$('.more a').each(function(){//.more aは以下のような制御を持つ
-					$('.more a').find('.more a').on('click', function(){
-								$(this).find('.more a').hide();
-								$(this).find('.more a').nextAll().slideDown();
-								return false;
-						});
-						$(this).find('.close a').on('click', function(){
-								$(this).find('.more a').nextAll.slideUp('slow', function(){
-										$(this).find('.more a').show();
-								});
-								return false;
-						});
-				});
+	$('.list').each(function(){//.listは以下のような制御を持つ
+		$('.list').find('li:(3)').hide();//li0-3を表示させ、他のliは隠す
+		return false;//出来るまで繰り返す
+	});
+	$('.list li more a').each(function(){//.list li more aは以下のような制御を持つ
+		$('.list li more a').on('click', function(){//.list li more aをクリックすると以下の制御を持つ
+		$('.list li more a').find('li:gt(2), li:gt(2)').hide();//li0-2,4-7を表示させる。li3だけ隠す。
+		$(this).find('.more a').nextAll().slideDown();//li4-7をSlideDownで表示させる
+		return false;//出来るまで繰り返す
+	});
+	$(this).find('.close a').on('click', function(){//.list li close aは以下のような制御を持つ
+		$(this).find('.more a').nextAll.slideUp('slow', function(){//.list li close aをクリックすると以下の制御を持つ
+			$(this).find('.more a').show();//li4-7を隠す
+		});
+	return false;
+	});
 });
 */
-
-$(function(){
-		$('.list').each(function(){//.listは以下のような制御を持つ
-				$('.list').find('li:(3)').hide();//li0-3を表示させ、他のliは隠す
-				return false;//出来るまで繰り返す
-		});
-		$('.list li more a').each(function(){//.list li more aは以下のような制御を持つ
-				$('.list li more a').on('click', function(){//.list li more aをクリックすると以下の制御を持つ
-						$('.list li more a').find('li:gt(2), li:gt(2)').hide();//li0-2,4-7を表示させる。li3だけ隠す。
-						$(this).find('.more a').nextAll().slideDown();//li4-7をSlideDownで表示させる
-						return false;//出来るまで繰り返す
-				});
-				$(this).find('.close a').on('click', function(){//.list li close aは以下のような制御を持つ
-						$(this).find('.more a').nextAll.slideUp('slow', function(){//.list li close aをクリックすると以下の制御を持つ
-								$(this).find('.more a').show();//li4-7を隠す
-						});
-						return false;
-				});
-		});
-});
-
 /*
 $(function(){
 		$(".more").on('click',function(){
