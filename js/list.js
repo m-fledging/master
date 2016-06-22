7aa2aa5a2260deb6e869a519109768c59eb86c98
michio <highberg525@gmail.com>
2016-06-22 20:49:49 +0900

    「もっと見る」の件

--- js/list.js
+++ js/list.js
@@ -28,47 +28,49 @@ $(function(){
 
 
 //	以下はhtmlに「もっと見る」「閉じる」を書いてjQueryにはその制御のみ書くパターン
-
+/*
 $(function(){
 		$(".list").each(function(){//.listは以下のような制御を持つ
 				$(".list").find("li:gt(2)").hide();//.listからliを３行表示させ、他のliは隠す
-				return false;//出来るまで繰り返す
+				return false;//出来るまで繰り返す これ、いる？
 				});
 		$(".more").each(function(){//.moreは以下のような制御を持つ
 				$(".more").on('click', function(){//.moreをクリックすると以下の制御を持つ
+						$(".more").find("li:gt(5)");//.moreを隠す
 						$(".more").find("li:gt(5)");//liを６行表示させる
 						$(".more").find(".close");//.closeを表示させる
 						return false;//出来るまで繰り返す
 						});
-				$(".more").find(".close").on('click', function(){
-						$(this).find('.more').nextAll.slideUp('slow', function(){
-								$(this).find('.more').show();
+				$(".more").find(".close").on('click', function(){//.closeをクリックすると以下の制御を持つ
+						$(this).find('.more').nextAll.slideUp('slow', function(){//.closeを隠す
+								$(this).find('.more').show();//
 						});
 								return false;
 						});
 				});
 });
-
-/*
-$(function(){
-	$('.list').each(function(){//.listは以下のような制御を持つ
-		$('.list').find('li:(3)').hide();//li0-3を表示させ、他のliは隠す
-		return false;//出来るまで繰り返す
-	});
-	$('.list li more a').each(function(){//.list li more aは以下のような制御を持つ
-		$('.list li more a').on('click', function(){//.list li more aをクリックすると以下の制御を持つ
-		$('.list li more a').find('li:gt(2), li:gt(2)').hide();//li0-2,4-7を表示させる。li3だけ隠す。
-		$(this).find('.more a').nextAll().slideDown();//li4-7をSlideDownで表示させる
-		return false;//出来るまで繰り返す
-	});
-	$(this).find('.close a').on('click', function(){//.list li close aは以下のような制御を持つ
-		$(this).find('.more a').nextAll.slideUp('slow', function(){//.list li close aをクリックすると以下の制御を持つ
-			$(this).find('.more a').show();//li4-7を隠す
-		});
-	return false;
-	});
-});
 */
+
+$(function(){//開始１
+	$('.list').each(function(){//.listは以下のような制御を持つ	開始２
+		$('.list').show('li:(2)').hide();//li1-3を表示させ、他のliは隠す
+		$('.list .more a').each(function(){//.list .more aは以下のような制御を持つ	開始３
+			$('.list .more a').on('click', function(){//.list .more aをクリックすると以下の制御を持つ	開始４
+				$('.list .more a').hide();//.list .more aを隠す
+				$('.list .more a').show('li:gt(5)').nextAll().slideDown();//li1-6を表示させる。
+				$('.list .more a').show('.list .close a');//.list .close aを表示させる。
+			});//閉じ４
+		});//閉じ３
+		$('.list .close a').on('click', function(){//.list .close aをクリックすると以下の制御を持つ	開始５
+			$('.list .close a').hide();//.list .close aを隠す
+			$('.list .close a').hide('li:gt(4, 5, 6)').slideUp();//スライドアップでli4-6を隠す
+			$('.list .close a').show('.list .more a');//「もっと見る」を表示させる。
+		});//閉じ５
+	});//閉じ２
+});//閉じ１
+
+
+		
 /*
 $(function(){
 		$(".more").on('click',function(){
